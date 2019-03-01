c4bb56bf22a4e171f14008a45c9f4d4baa4ac7eb
jibin varghese <jibinskpm123@gmail.com>
2019-03-01 11:46:23 +0530

    changed allignment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,21 +1,21 @@
+# Post controller class
 class PostsController < ApplicationController
- def new
- end
- def show
- 	@post = Post.find(params[:id])
- end
-def create 
-  #render plain: params[:post].inspect
-  @post = Post.new(post_params)
- 
-  @post.save
-  redirect_to @post
-end
- 
-private
+  def new; end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def create
+    # render plain: params[:post].inspect
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to @post
+  end
+
+  private
+
   def post_params
     params.require(:posts).permit(:title, :text)
-end
-
-	
+  end
 end
--- db/migrate/20190228110054_create_posts.rb
+++ db/migrate/20190228110054_create_posts.rb
@@ -1,3 +1,4 @@
+# createpost
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
