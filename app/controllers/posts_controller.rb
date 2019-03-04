0a80974d6fcee4e6e1ab5cd34f678f0e6436b79a
jibin varghese <jibinskpm123@gmail.com>
2019-03-04 12:44:36 +0530

    update feature

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,21 +1,44 @@
 # Post controller class
 class PostsController < ApplicationController
-  def new; end
+  def index
+    @post = Post.all
+   end
+
+  def new
+    @post = Post.new
+ end
+
+  def edit
+    @post = Post.find(params[:id])
+ end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+ end
 
   def show
     @post = Post.find(params[:id])
   end
 
   def create
-    # render plain: params[:post].inspect
     @post = Post.new(post_params)
-    @post.save
-    redirect_to @post
+
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
   end
 
   private
 
   def post_params
-    params.require(:posts).permit(:title, :text)
+    params.require(:post).permit(:title, :text)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,4 @@
+# post
 class Post < ApplicationRecord
+  validates :title, presence: true, length: { minimum: 5 }
 end
