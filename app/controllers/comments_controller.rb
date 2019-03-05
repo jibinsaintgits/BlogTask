393e02e54696434c8b78238c45e8f443b3eb4407
jibinsaintgits <37616574+jibinsaintgits@users.noreply.github.com>
2019-03-05 12:26:42 +0530

    Update comments_controller.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,13 @@
 class CommentsController < ApplicationController
-	 def create
+def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(comment_params)
     redirect_to post_path(@post)
-  end
+end
  
-  private
-    def comment_params
-      params.require(:comment).permit(:commenter, :body)
-    end
+private
+	
+ def comment_params
+   params.require(:comment).permit(:commenter, :body)
  end
+end
