e79827c371c65ac2d6c95786a5194ef6f1cb45bd
jibin varghese <jibinskpm123@gmail.com>
2019-03-05 15:38:16 +0530

    Deleting spam comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,20 @@
+# commentsController
 class CommentsController < ApplicationController
-	 def create
+  def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(comment_params)
     redirect_to post_path(@post)
   end
- 
+    def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+    redirect_to post_path(@post)
+  end
+
   private
-    def comment_params
-      params.require(:comment).permit(:commenter, :body)
-    end
- end
+
+  def comment_params
+    params.require(:comment).permit(:commenter, :body)
+  end
+end
