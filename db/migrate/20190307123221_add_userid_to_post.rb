958cf42da99c9c9302049f41fabdd7b4ef36f927
jibin varghese <jibinskpm123@gmail.com>
2019-03-07 18:51:54 +0530

    Alligmenet fixed

--- db/migrate/20190307123221_add_userid_to_post.rb
+++ db/migrate/20190307123221_add_userid_to_post.rb
@@ -1,6 +1,9 @@
+# frozen_string_literal: true
+
+# class
 class AddUseridToPost < ActiveRecord::Migration[5.2]
   def change
-  	add_reference :posts, :user, index: true
-  	add_foreign_key :posts, :users
+    add_reference :posts, :user, index: true
+    add_foreign_key :posts, :users
   end
 end
