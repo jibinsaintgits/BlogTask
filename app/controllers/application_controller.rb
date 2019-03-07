26c183d4d8b63efce7d8bb18e329d19e0fce5b8f
jibin varghese <jibinskpm123@gmail.com>
2019-03-07 18:42:10 +0530

    User authorisation feature added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
+# frozen_string_literal: true
+
 # Application controller
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: %i[index show]
 end
